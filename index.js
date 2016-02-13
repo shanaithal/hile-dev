829b31c4d3c0d0fd6a4f5fffab9a54950d7c0fe3
techpool <suryadeep10@gmail.com>
2016-02-13 20:30:54 +0530

    port restored

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 3000;
 
 var app = new express();
 
